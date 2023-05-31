e67ac42bdf944b9bd410324ab74e482ecd514f55
Joseph NOMO <fulgaros@gmail.com>
2023-05-31 15:43:15 +0100

    entry configuration

---
+++ lua/plugins/modules/packages.lua
@@ -0,0 +1,10 @@
+return {
+  { "simrat39/rust-tools.nvim" },
+  { 'wbthomason/packer.nvim' },
+  { "wakatime/vim-wakatime" },
+
+  { "akinsho/toggleterm.nvim",                     version = "^2" },
+  { "tzachar/cmp-tabnine", build = "./install.sh", dependencies = 'hrsh7th/nvim-cmp', },
+
+  { "jghauser/mkdir.nvim" },
+}
